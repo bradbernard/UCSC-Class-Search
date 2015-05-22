4239207f2806a9cf4239f2384e2da499474491d3
Brad Bernard <bradbernard@me.com>
2015-05-21 21:22:37 -0700

    First commit

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,222 @@
+<?php namespace App\Http\Controllers;
+
+use Twilio;
+
+class SearchController extends Controller {
+
+   function searchMine()
+   {
+
+      $options = [
+
+         [
+            'class' => [
+
+               'name'            => 'CMPE 12 - 01',
+               'teacherFull'     => 'Dunne,M.J.',
+               'time'            => '08:00AM-09:45AM',
+               'days'            => 'TuTh',
+               'type'            => 'LEC',
+               'location'        => 'Humn Lecture Hall',
+               'credits'         => '5',
+               'teacherShort'    => 'Dunne',
+               'subjectShort'    => 'CMPE',
+               'matches'         => '5'
+            
+            ],
+            'number' => getenv('BRAD_NUMBER'),
+            'termId' => 2158
+         ],
+         [
+            'class'  => [
+
+               'name'            => 'CMPS 5J - 01',
+               'teacherFull'     => 'Tantalo,P.',
+               'time'            => '05:00PM-06:45PM',
+               'days'            => 'MW',
+               'type'            => 'LEC',
+               'location'        => 'Media Theater M110',
+               'credits'         => '5',
+               'teacherShort'    => 'Tantalo',
+               'subjectShort'    => 'CMPS',
+               'matches'         => '6'
+
+            ],
+            'number' => getenv('MATT_NUMBER'),
+            'termId' => 2158
+         ]
+
+      ];
+
+      foreach($options as $option)
+      {
+         $this->searchClass($option);
+      }
+      
+   }
+
+   function searchClass($options)
+   {
+
+      $number  = $options['number'];
+      $class   = $options['class'];
+      $termId  = $options['termId'];
+
+      //$class = [
+      //
+      //   'name'            => 'CMPE 12 - 01',
+      //   'teacherFull'     => 'Dunne,M.J.',
+      //   'time'            => '08:00AM-09:45AM',
+      //   'days'            => 'TuTh',
+      //   'type'            => 'LEC',
+      //   'location'        => 'Humn Lecture Hall',
+      //   'credits'         => '5',
+      //   'teacherShort'    => 'Dunne',
+      //   'subjectShort'    => 'CMPE',
+      //   'matches'         => '5'
+      //
+      //];
+
+      //$class = [
+      //
+      //   'name'            => 'CMPS 129 - 01',
+      //   'teacherFull'     => 'Long,D.D.',
+      //   'time'            => '03:30PM-04:40PM',
+      //   'days'            => 'MWF',
+      //   'type'            => 'LEC',
+      //   'location'        => 'J Baskin Engr 165',
+      //   'credits'         => '5',
+      //   'teacherShort'    => 'Long',
+      //   'subjectShort'    => 'CMPS',
+      //   'matches'         => '6'
+      //
+      //];
+
+      //$class = [
+      //
+      //   'name'            => 'CMPS 280S - 01',
+      //   'teacherFull'     => 'Long,D.D.',
+      //   'time'            => '01:00PM-03:00PM',
+      //   'days'            => 'M',
+      //   'type'            => 'SEM',
+      //   'location'        => 'Engineer 2 599',
+      //   'credits'         => '2',
+      //   'teacherShort'    => 'Long',
+      //   'subjectShort'    => 'CMPS',
+      //   'matches'         => '6'
+      //
+      //];
+
+      $body = [
+
+         'action'                      => 'results',
+         'binds[:term]'                => $termId,
+         'binds[:reg_status]'          => 'all',
+         'binds[:subject]'             => $class['subjectShort'],
+         'binds[:instr_name_op]'       => 'contains',
+         'binds[:instructor]'          => $class['teacherShort'],
+         'binds[:crse_units_exact]'    => $class['credits']
+
+      ];
+
+      $options = [
+
+         'body' => $body
+
+      ];
+
+      $client = new \GuzzleHttp\Client();
+      $response = $client->post('https://pisa.ucsc.edu/class_search/index.php', $options);
+
+      $html = new \Htmldom();
+      $html->load($response->getBody());
+
+      foreach($html->find('#results_table tbody') as $tbody)
+      {
+         foreach($tbody->find('tr') as $trow)
+         {
+            foreach($trow->find('td') as $tdetail)
+            {
+               if($tdetail->plaintext == $class['name'])
+               {
+                  if($this->validateClass($trow, $class))
+                  {
+                     $seats = $this->openSeats($trow, $class);
+
+                     if($seats['availableSeats'] > 0)
+                     {
+                        if($this->sendText($number, $class, $seats, $trow))
+                        {
+                           echo 'Sent text to ' . $number . '. There are ' . $seats['availableSeats'] . ' open seats in ' . $class['name'] . ".\n";
+
+                           break 3;
+                        }
+                     }
+                  }
+               }
+            }
+         }
+      }
+   }
+
+   function validateClass($trow, $class)
+   {
+      $matches = 0;
+      //$index = 0;
+      $requiredMatches = $class['matches'];
+
+      foreach($trow->find('td') as $tdetail)
+      {
+         if(in_array($tdetail->plaintext, $class, true))
+         {
+            $matches++;
+         }
+
+         //echo "<pre>", ($index++), ': ', $tdetail->plaintext, "</pre><br/>";
+      }
+
+      if($matches == $requiredMatches)
+      {
+         return true;
+      }
+
+      return false;
+   }
+
+   function openSeats($trow, $class)
+   {
+      $capacity            = $trow->find('td', 8)->plaintext;
+      $enrollmentTotal     = $trow->find('td', 9)->plaintext;
+      $availableSeats      = $trow->find('td', 10)->plaintext;
+
+      //echo "$capacity $enrollmentTotal $availableSeats <br/>";
+
+      return [
+
+         'availableSeats'        => $availableSeats,
+         'enrollmentTotal'       => $enrollmentTotal,
+         'capacity'              => $capacity
+
+      ];
+   }
+
+   function sendText($number, $class, $seats, $trow)
+   {
+
+      $twilio = Twilio::from('twilio');
+
+      $message     = 'There are ' . $seats['availableSeats'] . ' available seats!' . "\n";
+      $message    .= 'Seats Filled: ' . $seats['enrollmentTotal'] . '/' . $seats['capacity'] . "\n";
+      $message    .= 'Class: ' . $class['name'] . "\n";
+      $message    .= 'Name: ' . $trow->find('td', 2)->plaintext . "\n";
+      $message    .= 'Teacher: ' . $class['teacherFull'] . "\n";
+      $message    .= 'Location: ' . $class['location'] . "\n";
+      $message    .= 'Days: ' . $class['days'] . "\n";
+      $message    .= 'Time: ' . $class['time'] . "\n";
+      $message    .= 'Type: ' . $class['type'] . "\n";
+      $message    .= 'Credits: ' . $class['credits'] . "\n";
+
+      return $twilio->message($number, $message);
+   }
+
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,43 @@
+<?php
+
+//use App\Http\Controllers\SearchController;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+//Route::get('/', function()
+//{
+//
+//   $searchController = new SearchController();
+//
+//   $class = [
+//
+//      'name'            => 'CMPS 280S - 01',
+//      'teacherFull'     => 'Long,D.D.',
+//      'time'            => '01:00PM-03:00PM',
+//      'days'            => 'M',
+//      'type'            => 'SEM',
+//      'location'        => 'Engineer 2 599',
+//      'credits'         => '2',
+//      'teacherShort'    => 'Long',
+//      'subjectShort'    => 'CMPS',
+//      'matches'         => '6'
+//
+//   ];
+//
+//   $number = "+17146550347";
+//   $termId = 2158;
+//
+//   return $searchController->searchClass($number, $class, $termId);
+//
+//});
+
+Route::get('/mine', ['uses' => 'SearchController@searchMine']);
