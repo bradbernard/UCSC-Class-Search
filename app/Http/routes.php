fbe9e9d5fa7182c72d63c60ac7d1e59b89407e53
Brad Bernard <bradbernard@me.com>
2015-05-30 16:51:37 -0700

    Commented closure

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\SmsResponseController;
+//use App\Http\Controllers\SmsResponseController;
 
 Route::get('/config', ['uses' => 'InsertController@getConfig']);
 Route::post('/sms', ['uses' => 'SmsResponseController@postSms']);
 
-Route::get('/', function()
-{
-
-   $controller = new SmsResponseController();
-   return $controller->parseBody('+17146550347', 'terms');
-
-});
+//Route::get('/', function()
+//{
+//
+//   $controller = new SmsResponseController();
+//   return $controller->parseBody('+17146550347', 'terms');
+//
+//});
