907a4cdf6b3462dd66cfabb48e41099462c4026a
Brad Bernard <bradbernard@me.com>
2015-05-30 23:29:47 -0700

    removed insert route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,4 +2,3 @@
 
 Route::get('/config', ['uses' => 'InsertController@getConfig']);
 Route::post('/sms', ['uses' => 'SmsResponseController@postSms']);
-Route::get('/', ['uses' => 'NotifyController@checkClasses']);
